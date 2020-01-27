48d6a2e0890f133bf2d54fbd64ad85287048f27b
monkeymr014 <monkeymr014@gmail.com>
2020-01-27 15:16:34 +0100

    małe poprawki

--- js_podctawy_EDU/losowanie/js/scripts.js
+++ js_podctawy_EDU/losowanie/js/scripts.js
@@ -23,7 +23,7 @@ sbt.addEventListener("click", function(){
         for(var i = 0 ; i < hm ; i++){
                 los = Math.floor(Math.random() * (maxv - minv + 1) + minv);
 
-                    var text = document.createTextNode(los + "|");
+                    var text = document.createTextNode("|" + los + "|");
                     var li = document.createElement("li");
                     li.classList.add('list-group-item');
                     li.appendChild(text);
@@ -53,8 +53,46 @@ sbt.addEventListener("click", function(){
                     },false);
         },false ) ;
                 
-                
-                
+        
+        const docStyle = document.documentElement.style
+        const aElem = document.querySelector('button')
+        const boundingClientRect = aElem.getBoundingClientRect()
+        
+        aElem.onmousemove = function(e) {
+        
+            const x = e.clientX - boundingClientRect.left
+            const y = e.clientY - boundingClientRect.top
+            
+            const xc = boundingClientRect.width/2
+            const yc = boundingClientRect.height/2
+            
+            const dx = x - xc
+            const dy = y - yc
+            
+            docStyle.setProperty('--rx', `${ dy/-1 }deg`)
+            docStyle.setProperty('--ry', `${ dx/10 }deg`)
+            
+        }
+        
+        aElem.onmouseleave = function(e) {
+            
+            docStyle.setProperty('--ty', '0')
+            docStyle.setProperty('--rx', '0')
+            docStyle.setProperty('--ry', '0')
+            
+        }
+        
+        aElem.onmousedown = function(e) {
+            
+            docStyle.setProperty('--tz', '-25px')
+            
+        }
+        
+        document.body.onmouseup = function(e) {
+            
+            docStyle.setProperty('--tz', '-12px')
+            
+        }
 
                                             
                 
