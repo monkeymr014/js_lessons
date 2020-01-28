144c68a7bca8cbe0be515ac722442344b797953d
monkeymr014 <monkeymr014@gmail.com>
2020-01-28 15:20:12 +0100

    --

--- js_podctawy_EDU/losowanie/js/scripts.js
+++ js_podctawy_EDU/losowanie/js/scripts.js
@@ -2,17 +2,13 @@
 
 var sbt = document.querySelector("#getNumbers");
 
-
-
 sbt.addEventListener("click", function(){
-
      
     var hmNumbers = document.querySelector("#hmNumbers").value;
     var min = document.querySelector("#minV").value;
     var max = document.querySelector("#maxV").value;
 
     hm = parseInt(hmNumbers);
-
     maxv = parseInt(max);
     minv = parseInt(min);
 
@@ -31,8 +27,8 @@ sbt.addEventListener("click", function(){
                     ul.appendChild(dc);
                     divul.appendChild(ul)
                 }
+                
         sbt.setAttribute("disabled","true");
-
                     var divbutton = document.querySelector("#button");
                     var reset = document.createElement("button");
                     text2 = document.createTextNode("Reset")
@@ -54,45 +50,7 @@ sbt.addEventListener("click", function(){
         },false ) ;
                 
         
-        const docStyle = document.documentElement.style
-        const aElem = document.querySelector('button')
-        const boundingClientRect = aElem.getBoundingClientRect()
         
-        aElem.onmousemove = function(e) {
-        
-            const x = e.clientX - boundingClientRect.left
-            const y = e.clientY - boundingClientRect.top
-            
-            const xc = boundingClientRect.width/2
-            const yc = boundingClientRect.height/2
-            
-            const dx = x - xc
-            const dy = y - yc
-            
-            docStyle.setProperty('--rx', `${ dy/-1 }deg`)
-            docStyle.setProperty('--ry', `${ dx/10 }deg`)
-            
-        }
-        
-        aElem.onmouseleave = function(e) {
-            
-            docStyle.setProperty('--ty', '0')
-            docStyle.setProperty('--rx', '0')
-            docStyle.setProperty('--ry', '0')
-            
-        }
-        
-        aElem.onmousedown = function(e) {
-            
-            docStyle.setProperty('--tz', '-25px')
-            
-        }
-        
-        document.body.onmouseup = function(e) {
-            
-            docStyle.setProperty('--tz', '-12px')
-            
-        }
 
                                             
                 
--- js_podctawy_EDU/walidacja_formularzy/js/scripts.js
+++ js_podctawy_EDU/walidacja_formularzy/js/scripts.js
@@ -1,39 +1,52 @@
 
     var form = document.querySelector("#myForm"),
-        fields = form.querySelectorAll("[data-error]");
+        fields = form.querySelectorAll("[data-error]"),
+        errordiv = document.querySelector("#error");
 
 
-        form.addEventListener("submit",function(e) {
+
+        function isEmpty(field){
+
+            return field.value === "" ;
+        }
+
+        form.addEventListener("submit",function(e) {    
             e.preventDefault();
-
             var errArr = [];
-
+            
             for(var i = 0; i < fields.length; i++){
-
+                
                 field = fields[i];
-                //field.push(errArr);
+                isValid = false;
+                field.classList.remove("error");
 
                 if(field.type === "text" && field.value == ""  ){
+                    
+                    isValid = isEmpty(field);
 
-                    errArr.push(field.dataset.error);
+
                 } else  if(field.type === "email"  && field.value.indexOf('@') === -1 ) 
                 {
-                    errArr.push(field.dataset.error);
+                    isValid = isEmpty(field);
                 } else if(field.name === "your-subject" && field.value === "")
                 {
-                    errArr.push(field.dataset.error);
+                    isValid = isEmpty(field);
                 }else if(field.type === "textarea" && field.value === "")
                 {
+                    isValid = isEmpty(field);
+                }
+
+
+                if(isValid){
+                    field.classList.add("error");
                     errArr.push(field.dataset.error);
                 }
 
-                }
+            }
+            
+
+        },false);    
         
-            console.log(errArr);
-
-
-    },false);    
-    
 
 
 
