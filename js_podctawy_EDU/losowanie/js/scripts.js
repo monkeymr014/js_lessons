1be8a18cce076185261089bfd4fddb46e9905faa
monkeymr014 <monkeymr014@gmail.com>
2020-02-17 14:10:08 +0100

    losowanie poprawki

--- js_podctawy_EDU/losowanie/js/scripts.js
+++ js_podctawy_EDU/losowanie/js/scripts.js
@@ -1,40 +1,59 @@
 var addRang = function (){
   var zakres = {}
-  zakres.min = function (){
-    var minv = document.querySelector("#minV").value;
-    return minv;
-  };
-  zakres.max = function (){
-    var maxv = document.querySelector("#maxV").value;
-    return maxv;
-  };
+    zakres.min = function (){
+        var minv = document.querySelector("#minV").value;
+        return minv;
+    };
+    zakres.max = function (){
+        var maxv = document.querySelector("#maxV").value;
+        return maxv;
+    };
   return zakres;
 };
+
+
+ var howMuch = function () {
+        this.hm =  document.querySelector("#hmNumbers").value;
+            return this.hm;
+        };   
 
 
 function losowanie(los){
     var arr = [];
     var z = new addRang;
+    var m = new howMuch;
+    var j = m.hm;
     var ma = z.max();
     var mi = z.min();
-    console.log(ma,mi);
-      for(var i = 0 ; i < 2; i++){
-      var   los = Math.floor(Math.random() * (ma - mi + 1) + mi);
-         console.log(los);
-         arr.push([los]);
-    };
-  };  //return Math.floor(Math.random() * (max - min)) + min;
-
-    
-    var sbt = document.querySelector("#getNumbers").addEventListener("click", function(){
-      var z = new addRang;
-
-      console.log(z.min(),z.max());
-
-      losowanie();
    
-      
+    for(var i = 0 ; i < j; i++){
+      var   los = Math.floor(Math.random() * (ma - mi + 1) + mi);
+         arr.push(los);
+    };
 
+    function compareNr(a, b) {
+        return a - b
+    }
+        return arr.sort(compareNr);
+  }; 
+
+function addWinLos () {
+
+ 
+  for(var i = 0 ; i < losowanie(los.arr).length; i ++){
+
+    console.log(losowanie(los.arr));    
+
+  };
+
+
+};
+
+
+
+var sbt = document.querySelector("#getNumbers").addEventListener("click", function(){
+      losowanie();
+      addWinLos();
 
   },false ) ;
   
